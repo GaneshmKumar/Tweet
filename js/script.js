5c9fe311dd166e8b59214a2347dc3d0f107c78c7
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2017-04-25 14:37:36 +0530

    Removed unwanted comments

--- js/script.js
+++ js/script.js
@@ -2,47 +2,10 @@
 * @Author: GaNeShKuMaRm
 * @Date:   2017-04-24 22:32:40
 * @Last Modified by:   GaNeShKuMaRm
-* @Last Modified time: 2017-04-25 13:59:22
+* @Last Modified time: 2017-04-25 14:37:24
 */
 
 'use strict';
-
-
-/*function process(current)
-{
-    var currentDomain = current.split('//')[1].split('/')[0];
-    localStorage["current"] = current;
-    if(currentDomain == "mail.google.com" || currentDomain == "www.gmail.com")
-    {
-        chrome.tabs.update({url: "login.html"});
-        window.close();
-    }
-}
-
-function getCurrentTab(e) {
-    chrome.tabs.query({ currentWindow: true, active: true}, function (tabs) {
-        var current = tabs[0].url;
-        process(current);
-    });
-}
-
-chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-       getCurrentTab();
-});
-
-document.getElementById('login-form').addEventListener('submit', function(e) {
-    var password = document.getElementById('password').value;
-    if(password == 'ganesh')
-    {
-        chrome.tabs.update({url: localStorage["current"]});
-    }
-    else
-    {
-        alert('Wrong Password');
-    }
-});
-*/
-
 chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
 }, function(selection) {
